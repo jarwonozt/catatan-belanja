26fd6500d4aeade2c55f221c8be9dc098678836c
jarwonozt <jarwonoztaveiro@gmail.com>
2024-04-19 18:41:54 +0700

    feat: add state input

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const groceryItems = [
   {
     id: 1,
@@ -35,6 +37,23 @@ function Header() {
 }
 
 function Form() {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!name) return; //stop submit if name === false
+
+    const newItem = {name, quantity, checked: false, id:Date.now()};
+  
+    console.log(newItem);
+  
+    setName('');
+    setQuantity(1);
+  }
+
+
   const quantityNum = [...Array(20)].map((_, i) => (
     <option value={i + 1} key={i + 1}>
       {i + 1}
@@ -42,11 +61,11 @@ function Form() {
   ));
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>{quantityNum}</select>
-        <input type="text" placeholder="nama barang..." />
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
       </div>
       <button>Tambah</button>
     </form>
